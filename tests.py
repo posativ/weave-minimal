07eff7bdf2b6d1447b9cf20492d4469c768f7088
posativ <info@posativ.org>
2011-12-05 22:33:21 +0100

    moar

--- tests.py
+++ tests.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-import sys
-
 from werkzeug.test import Client
 from weave import application
 from werkzeug.wrappers import BaseResponse
 
 from werkzeug.test import EnvironBuilder
-
 from base64 import standard_b64encode
-
-builder = EnvironBuilder('/', method='GET')
-env = builder.get_environ()
 
 c = Client(application, BaseResponse)
 
@@ -25,12 +19,7 @@ def test(method, path, data=None, headers={}, auth=None):
     print method, path, resp.status_code, `resp.data`
 
 if __name__ == '__main__':
-    
-    if not (len(sys.argv) > 1 and sys.argv[1] == '-f'):
-        f = True
-    else:
-        f = False
-    
+        
     test('GET', '/')
     test('GET', '/user/1.0/posativ')
     test('GET', '/user/1.0/other')
@@ -38,17 +27,25 @@ if __name__ == '__main__':
     
     test('GET', '/user/1.0/posativ/node/weave')
     test('GET', '/1.1/posativ/info/collections', auth=('posativ', 'test'))
-    test('GET', '/1.1/posativ/storage/meta/global', auth=('posativ', 'test'))
-    if f:
-        test('PUT', '/1.1/posativ/storage/meta/global', auth=('posativ', 'test'),
-            data='{"payload":"{\\"syncID\\":\\"eKZaDmY-LbUa\\",\\"storageVersion\\":5}","id":"global"}')
-        test('PUT', '/1.1/posativ/storage/meta/global', auth=('posativ', 'test'),
-            data='{"payload":"{\\"syncID\\":\\"eKZaDmY-LbUa\\",\\"storageVersion\\":5}","id":"global", "ttl":"42"}')
-    test('DELETE', '/1.1/posativ/storage/clients', auth=('posativ', 'test'))
+    
+    test('GET', '/1.1/posativ/storage/meta/global')
+    test('PUT', '/1.1/posativ/storage/meta/global',
+        data='{"payload":"qwertz","id":"global"}')
+    test('PUT', '/1.1/posativ/storage/meta/global',
+        data='{"payload":"asdf","id":"global", "ttl":"42"}')
+    test('POST', '/1.1/posativ/storage/meta',
+         data='[{"payload":"asdf","id":"global", "ttl":"42"}, {"payload":"asdf","id":"tmp", "ttl":"42"}]')    
+    
+    test('DELETE', '/1.1/posativ/storage/meta/tmp')
+    test('DELETE', '/1.1/posativ/storage/meta')
+    
     test('GET', '/1.1/posativ/info/collections', auth=('posativ', 'test'))
     test('GET', '/1.1/posativ/storage/meta/global', auth=('posativ', 'test'))
-    if f:
-        test('POST', '/1.1/posativ/storage/meta', auth=('posativ', 'test'), data=
-            '''[{"payload":"{\\"syncID\\":\\"eKZaDmY-LbUa\\",\\"storageVersion\\":5}","id":"global", "ttl":"42"},
-                {"payload":"{lala}","id":"test", "ttl":"23"}]''')
-#    test('DELETE', '/user/1.0/posativ', auth=('posativ', 'test'))
+    
+    for i in range(1, 4):
+        test('PUT', '/1.1/posativ/storage/test/%s' % i, data='{"payload": "test%s","id":"%s"}' % (i, i))
+    test('DELETE', '/1.1/posativ/storage/test?ids=1,3')#, auth=('posativ', 'test'))
+    test('GET', '/1.1/posativ/storage/test?ids=1,2&limit=10&sort=index&full=true', auth=('posativ', 'test'))
+    test('GET', '/1.1/posativ/storage/test', auth=('posativ', 'test'))
+    # XXX not implemented
+    # test('DELETE', '/user/1.0/posativ', auth=('posativ', 'test'))
