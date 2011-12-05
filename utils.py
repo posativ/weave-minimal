1a04038896a21c44ef9df61bd8aa6bf3e923a752
posativ <info@posativ.org>
2011-12-05 22:33:42 +0100

    wbo2dict and some docs

--- utils.py
+++ utils.py
@@ -8,7 +8,10 @@ from hashlib import sha1
 import sqlite3
 import time
 
+
 def path(dir, user, passwd):
+    """return joined path to database using data_dir + '/' + user.sha1(passwd)
+    -- a bit truncated though."""
     return join(dir, (user + '.' + sha1(passwd).hexdigest()[:16]))
 
 
@@ -39,10 +42,20 @@ class login:
         return dec
 
 
+def wbo2dict(res):
+    """converts sqlite table to WBO (dict [json-parsable])"""
+    return {'id': res[0], 'modified': round(res[1], 2),
+            'sortindex': res[2], 'payload': res[3], 'ttl': res[4]}
+        
+
+
 _FIELDS = ('id', 'username', 'collection', 'parentid',
            'predecessorid', 'sortindex', 'modified',
            'payload', 'payload_size')
 
+
+# XXX integrate into storage.py, some copypasta from
+# https://hg.mozilla.org/services/server-full/file/962c199ce0f4/1.1/
 
 class WeaveStorage:
     
