9f03b183eb0b8b53d5dfb5928e0af891323cbd96
posativ <info@posativ.org>
2012-08-31 13:15:58 +0200

    add server-full functional tests

---
+++ tests/test_config.py
@@ -0,0 +1,13 @@
+SCHEME = "http"
+SERVER_NAME = "localhost:5000"  #"dev-auth.services.mozilla.com"
+SERVER_BASE = "%s://%s" % (SCHEME, SERVER_NAME)
+
+STORAGE_SCHEME = None
+STORAGE_SERVER = None
+
+HOST_NAME = None
+USERNAME = None
+PASSWORD = "mypassword"
+SECRET = None
+
+memcache = True
