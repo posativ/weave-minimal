60522d0e7efc38b3d6f778e16f9210a4ae1560f4
posativ <info@posativ.org>
2012-09-05 16:41:36 +0200

    add uber captcha

--- controllers/misc.py
+++ controllers/misc.py
@@ -4,4 +4,5 @@
 from werkzeug import Response
 
 def captcha_html(environ, request, version):
-	return Response('CAPTCHA: set `ENABLE_REGISTER` to True, see controllers/user.py:30', 200)
+    return Response('<img src="http://posativ.org/scratch.png" \
+    width="298px" height="130" style="overflow-x: scroll; overflow-y: hidden;" />', content_type="text/html")
