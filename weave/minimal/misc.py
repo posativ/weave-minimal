80e44fe9904455daf002db9f69cd00cfbc804a7b
posativ <info@posativ.org>
2012-10-14 02:11:39 +0200

    fix captcha

--- weave/minimal/misc.py
+++ weave/minimal/misc.py
@@ -3,6 +3,6 @@
 
 from werkzeug import Response
 
-def captcha_html(environ, request, version):
+def captcha_html(app, environ, request, version):
     return Response('<img src="http://posativ.org/scratch.png" \
     width="298px" height="130" style="overflow-x: scroll; overflow-y: hidden;" />', content_type="text/html")
