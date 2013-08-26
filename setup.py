f4e226b2a8818e0607ee812b5145d896e1e6e4b6
Martin Zimmermann <info@posativ.org>
2013-08-26 14:51:19 +0200

    bump to 1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='weave-minimal',
-    version='1.0',
+    version='1.1',
     author='Martin Zimmermann',
     author_email='info@posativ.org',
     packages=find_packages(),
